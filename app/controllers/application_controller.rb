38f076422a8fdff12b7766dd06272621387747db
mike <mikeover719@gmail.com>
2015-10-05 22:58:22 -0400

    Add respond_to :json in the application_controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,8 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  respond_to :json
+
   # TODO: This should not be here, put it in a basic controller with a single action instead, this was just a quick way to get a route
   def angular
     render 'layouts/application'
